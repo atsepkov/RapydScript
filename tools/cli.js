49425686f12405f0d9708abdc8b01243f692cd60
Tsepkov <alexander.tsepkov@raq10-384.home>
2015-07-03 14:51:18 -0400

    Revert "CLI: Stop processing options after --"
    This reverts commit 50cea4e2ebf61b8ae3dfd4977210cbe4469129cd.

--- tools/cli.js
+++ tools/cli.js
@@ -226,12 +226,8 @@ function parse_args() {  // {{{
 		options.alias[name].forEach(function (alias) { name_map[alias] = name; });
 	});
 
-    var options_ended = false;
-
 	all_args.forEach(function(arg) {
-        if (options_ended) plain_arg(arg);
-        else if (arg === '--') options_ended = true;
-		else if (arg === '-') plain_arg(arg);
+		if (arg === '-') plain_arg(arg);
 
 		else if (arg[0] === '-') handle_opt(arg.substr(1));
 
